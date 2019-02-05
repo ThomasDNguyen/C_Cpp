bca9e82b33a653174e254e52771632bd977059d0
Nguyen Duy Thanh (Thomas) <yohowdoyoudo@gmail.com>
2019-02-05 10:36:00 +0200

    Update EmbeddedSystemDesign_HW_week5.4.c

--- EmbeddedSystemDesign_HW_week5.4.c
+++ EmbeddedSystemDesign_HW_week5.4.c
@@ -18,20 +18,20 @@ Problem:
 			http://www.wolframalpha.com/widgets/view.jsp?id=a278064e754d61cbecc14f913b8d5295
 For example:
 
-Test																| Result
+Test								  | Result
 ---------------------------------------------------------------------------------------------------------------
-double x_values[10]={1,2,3,4,5,6,7,8,9,10};							| SD=Symbolic Derivative
-double x0_values[10]={1.1,2.1,3.1,4.1,5.1,6.1,7.1,8.1,9.1,10.1};	| ND=Numerical Derivative
-int array_size=10;													| x=1.00 and SD=3.00 and ND=3.10
-centraldifference(x_values, x0_values, array_size);					| x=2.00 and SD=96.00 and ND=96.80
-																	| x=3.00 and SD=729.00 and ND=731.70
-																	| x=4.00 and SD=3072.00 and ND=3078.40
-																	| x=5.00 and SD=9375.00 and ND=9387.50
-																	| x=6.00 and SD=23328.00 and ND=23349.60
-																	| x=7.00 and SD=50421.00 and ND=50455.30
-																	| x=8.00 and SD=98304.00 and ND=98355.20
-																	| x=9.00 and SD=177147.00 and ND=177219.90
-																	| x=10.00 and SD=300000.00 and ND=300100.00
+double x_values[10]={1,2,3,4,5,6,7,8,9,10};			  | SD=Symbolic Derivative
+double x0_values[10]={1.1,2.1,3.1,4.1,5.1,6.1,7.1,8.1,9.1,10.1};  | ND=Numerical Derivative
+int array_size=10;						  | x=1.00 and SD=3.00 and ND=3.10
+centraldifference(x_values, x0_values, array_size);		  | x=2.00 and SD=96.00 and ND=96.80
+								  | x=3.00 and SD=729.00 and ND=731.70
+								  | x=4.00 and SD=3072.00 and ND=3078.40
+								  | x=5.00 and SD=9375.00 and ND=9387.50
+								  | x=6.00 and SD=23328.00 and ND=23349.60
+								  | x=7.00 and SD=50421.00 and ND=50455.30
+								  | x=8.00 and SD=98304.00 and ND=98355.20
+							          | x=9.00 and SD=177147.00 and ND=177219.90
+								  | x=10.00 and SD=300000.00 and ND=300100.00
 */
 
 #include <stdio.h>
