09e92833ab0beeeb0507ed3d190e731ffa4d6aff
Nguyen Duy Thanh (Thomas) <yohowdoyoudo@gmail.com>
2019-02-05 10:27:55 +0200

    Update EmbeddedSystemDesign_HW_week4.5.c

--- EmbeddedSystemDesign_HW_week4.5.c
+++ EmbeddedSystemDesign_HW_week4.5.c
@@ -8,19 +8,19 @@ Calculation can be tested with following calculator and two decimal accuracy can
 https://www.emathhelp.net/calculators/calculus-2/simpsons-rule-calculator/
 For example:
 
-Test				| Result
-------------------------------------------------
-int a=1;			| i=0 and f(x1.00)=1.00
-int b=16;			| i=1 and 4*f(x8.50)=289.00
-int n=2;			| i=2 and f(x16.00)=256.00
-simpson(a,b,n);		| result=1365.00
-------------------------------------------------
-int a=5;			| i=0 and f(x5.00)=25.00
-int b=18;			| i=1 and 4*f(x8.25)=272.25
-int n=4;			| i=2 and 2*f(x11.50)=264.50
-simpson(a,b,n);		| i=3 and 4*f(x14.75)=870.25
-					| i=4 and f(x18.00)=324.00
-					| result=1902.33
+Test		| Result
+--------------------------------------------
+int a=1;	| i=0 and f(x1.00)=1.00
+int b=16;	| i=1 and 4*f(x8.50)=289.00
+int n=2;	| i=2 and f(x16.00)=256.00
+simpson(a,b,n);	| result=1365.00
+--------------------------------------------
+int a=5;	| i=0 and f(x5.00)=25.00
+int b=18;	| i=1 and 4*f(x8.25)=272.25
+int n=4;	| i=2 and 2*f(x11.50)=264.50
+simpson(a,b,n);	| i=3 and 4*f(x14.75)=870.25
+		| i=4 and f(x18.00)=324.00
+		| result=1902.33
 */
 
 #include <stdio.h>
